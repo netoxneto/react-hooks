a2df6ecd402ac3ba5dd734ac976bc09e7056bdd5
netoxneto <netoxneto@hotmail.com>
2023-06-30 22:38:44 -0300

    At_index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './views/App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './views/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import App from './views/App'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
